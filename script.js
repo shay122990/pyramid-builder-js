4858bdd1f060fdb4e9bc25cd27740e40ea08a93a
Shay <moeshakasanova@gmail.com>
2024-04-22 23:25:40 +0400

    pyramid function

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow(rowNumber, rowCount) {
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
+}
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1));
+  rows.push(padRow(i + 1, count));
 }
 
 let result = "";
